1d4da7f50aaa4616a3ee4c91f81a69b531b0fdb3
Fabio Bressler <fabio.bressler@gmail.com>
2021-06-09 04:01:34 +0100

    Init commit

---
+++ src/netcheck.py
@@ -0,0 +1,9 @@
+"""
+Routine to check from time to time if network is behaving badly
+Can provide statistics for network quality
+Also, can be used to question ISP why network speed is not what is in contract
+"""
+
+
+
+
